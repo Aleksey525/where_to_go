be26b20b4059cb8bce1ad259ad25acb830f8f8c7
Aleksey525 <alex_tolchin@mail.ru>
2024-07-09 02:07:25 +0300

    simplified adding photos to the admin panel

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Place, Image
 
-admin.site.register(Place)
+
+class ImageInline(admin.StackedInline):
+    model = Image
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
+
+
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Image)
