f12a29455cb6a7cb5c7323eecfa0bc21ef7aa04c
Aleksey525 <alex_tolchin@mail.ru>
2024-07-14 20:41:26 +0300

    removed the default SECRET_KEY value

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
