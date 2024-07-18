efb54b826ff69e0388bb36c0b6269f6781b83db8
Aleksey525 <alex_tolchin@mail.ru>
2024-07-18 17:45:23 +0300

    made the field mandatory

--- places/models.py
+++ places/models.py
@@ -19,7 +19,7 @@ class Place(models.Model):
 
 class Image(models.Model):
     img = models.ImageField(verbose_name='Фото')
-    place = models.ForeignKey(Place, null=True, related_name='images', on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, related_name='images', on_delete=models.CASCADE)
     position = models.PositiveIntegerField(verbose_name='Позиция', default=0, db_index=True)
 
     class Meta:
